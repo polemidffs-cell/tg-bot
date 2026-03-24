65d52d08317e3520f35817d5bc0973feb98724ec
polemidffs-cell <polemidffs@gmail.com>
2026-03-24 14:06:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 # ⚙️ НАСТРОЙКИ
 import os
-TOKEN = os.getenv("TOKENPLMD")
+TOKEN = os.getenv("TOKEN")
 SERVER_IP = ("46.174.54.177", 27015)  # IP и порт сервера
 
 # 📡 Получение информации о сервере
