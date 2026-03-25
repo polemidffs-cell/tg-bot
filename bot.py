8985133f92cd22005bdb6ad7569bb0eb684a5496
polemidffs-cell <polemidffs@gmail.com>
2026-03-25 08:55:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,18 @@
+import os
 import a2s
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.request import HTTPXRequest
 
 # ⚙️ НАСТРОЙКИ
-import os
 TOKEN = os.getenv("TOKEN")
-SERVER_IP = ("46.174.54.177", 27015)  # IP и порт сервера
+PROXY_URL = os.getenv("PROXY_URL")
+
+SERVER_IP = ("46.174.54.177", 27015)
+
+# 🌐 ПРОКСИ (пример)
+PROXY_URL = "http://dzk8gZ:qQtV1G@45.91.209.150:12005"
+# например: http://123:123@45.67.89.10:8080
 
 # 📡 Получение информации о сервере
 async def get_server_info():
@@ -47,7 +54,15 @@ async def status(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 # 🚀 Запуск бота
 if __name__ == "__main__":
-    app = ApplicationBuilder().token(TOKEN).build()
+    request = HTTPXRequest(proxy=PROXY_URL)
+
+    app = (
+        ApplicationBuilder()
+        .token(TOKEN)
+        .request(request)   # ← ВОТ ЭТО ГЛАВНОЕ
+        .build()
+    )
+
     app.add_handler(CommandHandler("status", status))
 
     print("Бот запущен...")
